d08a2943c423654447bdc6822de38845f5e9c8d2
Tonté Pouncil <pouncilt.developer@gmail.com>
2018-02-15 09:52:16 -0600

    Updated test by removing unused lines of code and add global variable comments.

--- src/test/javascript/oopSpec.js
+++ src/test/javascript/oopSpec.js
@@ -1,12 +1,12 @@
 /**
  * Created by tonte on 2/5/18.
  */
+/*global expect, describe, it*/
 define(['bytepushers'], function(BytePushers) {
     describe("Object Oriented Programming tests:", function() {
 
         describe('Namespaces', function () {
             it('should be created successfully', function() {
-                var testNameSpace = ['software', 'bytepushers', 'test'];
                 BytePushers.Test = BytePushers.namespace("software.test");
 
                 expect(BytePushers.Test).toBe(BytePushers.software.test);
